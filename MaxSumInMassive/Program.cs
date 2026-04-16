c04f9991e612f7b4cdc37f4906bb788401ab5a47
Timooff <timuffir2@gmail.com>
2026-04-16 13:24:27 +0300

    MaxSub task complete

---
+++ MaxSumInMassive/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    public static int SubSum(int[] mas){
+
+    int maxSum = 0;
+    int partialSum = 0;
+        foreach(int el in  mas){
+        partialSum += el;
+        maxSum = Math.Max(maxSum, partialSum);
+        if (partialSum < 0){partialSum = 0;}
+    }
+    return maxSum;
+}
+
+    
+    static void Main()
+    {
+
+        int[] mas = [1,2,-10,2, 5,-3,20,-3];
+        Console.WriteLine(SubSum(mas));
+
+}
+
+    }
+
+
